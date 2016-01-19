8e965fcbe38ab95fb29508a0f1da112677eea010
CubeKirito <cubekirito@cubesoft.asia>
2016-01-19 22:17:41 +0900

    Updated encoding of login/js/default.js (ANSI to UTF-8)

--- login/js/default.js
+++ login/js/default.js
@@ -9,11 +9,19 @@ function login() {
 
 	httpRequest.onreadystatechange = function() {
 		if(httpRequest.readyState == 4 && httpRequest.status == 200) {
+			var data = JSON.parse(httpRequest.responseText);
 			
+			if(data.RESULT == "LOGIN_SUCCESS") {
+				window.location.replace('../cloud/');
+			} else if(data.RESULT == "LOGIN_FAILED") {
+				alert('로그인에 실패하였습니다.');
+			} else {
+				alert('서버 문제로 로그인에 실패하였습니다.');
+			}
 		}
 	};
 	
-	httpRequest.open('POST', '../php/login.php', true);
+	httpRequest.open('POST', 'php/login.php', true);
 	httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	httpRequest.send("id=" + id + "&pw=" + pw);
 }
