e488db2d6c7f716bb6390772229f09a8899a695f
CubeKirito <cubekirito@cubesoft.asia>
2016-01-19 22:07:43 +0900

    Updated login/php/login.php

--- login/php/login.php
+++ login/php/login.php
@@ -1,18 +1,24 @@
 <?php
+	session_start();
 	$adminData = file_get_contents('../../db/Admins.db');
 	$guestData = file_get_contents('../../db/Guests.db');
+
+	$logined = false;
 	
 	if(!isset($_POST['id']) || !isset($_POST['pw'])) { $result['RESULT'] = "LOGIN_FAILED"; exit(json_encode($result)); }
 	$id = $_POST['id'];
 	$pw = sha1($_POST['pw']);
 
+
 	$admins = explode('\r\n', $adminData);
 
-	foreach($k as $admins) {
-		$data = explode('|', $admins);
-		
+	foreach($admins as $k) {
+		$data = explode('|', $k);
+
 		if($data[0] == $id && $data[1] == $pw) {
 			$_SESSION['logined'] = true;
+
+			$logined = true;
 
 			$_SESSION['id'] = $id;
 			$_SESSION['type'] = "admin";
@@ -22,18 +28,20 @@
 	
 	$guests = explode('\r\n', $guestData);
 
-	foreach($k as $guests) {
-		$data = explode('|', $guests);
+	foreach($guests as $k) {
+		$data = explode('|', $k);
 
 		if($data[0] == $id && $data[1] == $pw) {
 			$_SESSION['logined'] = true;
+
+			$logined = true;
 
 			$_SESSION['id'] = $id;
 			$_SESSION['type'] = "guest";
 		}
 	}
 	
-	if($_SESSION['logined']) $result['RESULT'] = "LOGIN_FAILED";
-	else $result['RESULT'] = "LOGIN_SUCCESS";
+	if($logined) $result['RESULT'] = "LOGIN_SUCCESS";
+	else $result['RESULT'] = "LOGIN_FAILED";
 	exit(json_encode($result));
 ?>
